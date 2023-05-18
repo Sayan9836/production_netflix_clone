b171ccae2ee0012d3ac1431a297cfedf0f4ad5cc
Sayan9836 <sayanmaity631@gmail.com>
2023-05-18 19:13:05 +0530

    sdas

--- nextflix-backend/server.js
+++ nextflix-backend/server.js
@@ -4,6 +4,20 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
 const app = express();
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://6465ed6a698478000855cf32--mellow-torrone-a30106.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      res.status(200).end();
+      return;
+    }
+    
+    next();
+  });
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../netflix-frontend/build')))
@@ -13,10 +27,7 @@ app.get('*', (req, res) => {
 const DB = "mongodb+srv://sayanmaity631:gdcgdufuB7GdUS0i@cluster0.0muzrkn.mongodb.net/netflix?retryWrites=true&w=majority";
 mongoose.connect(DB)
 
-app.use((req,res,next)=>{
- res.header('Access-Control-Allow-Origin', '*');
- next();
-})
+
 
 
 app.use("/api/user", userRoutes);
