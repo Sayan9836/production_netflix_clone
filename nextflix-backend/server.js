3514c973c527f32a2699eb89e741cc1533d74a8e
Sayan9836 <sayanmaity631@gmail.com>
2023-05-20 18:13:09 +0530

    dsadas

--- nextflix-backend/server.js
+++ nextflix-backend/server.js
@@ -25,7 +25,15 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../netflix-frontend/build/index.html'))
 });
 const DB = process.env.MONGO_URI;
-mongoose.connect(DB)    
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) =>
+    console.error("Error connecting to MongoDB Atlas:", error)
+  ); 
 
 
 app.use("/api/user", userRoutes);
