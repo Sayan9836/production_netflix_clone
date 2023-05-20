8c6b7f5c2dbee965e4a2bf618cff41fe86c997c7
Sayan9836 <sayanmaity631@gmail.com>
2023-05-20 15:48:51 +0530

    sadasd

--- nextflix-backend/server.js
+++ nextflix-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const userRoutes = require("./routes/UserRoutes");
 const app = express();
 
@@ -17,17 +18,14 @@ app.use(function(req, res, next) {
     
     next();
   });
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../netflix-frontend/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../netflix-frontend/build/index.html'))
 });
-const DB = "mongodb+srv://sayanmaity631:gdcgdufuB7GdUS0i@cluster0.0muzrkn.mongodb.net/netflix?retryWrites=true&w=majority";
-mongoose.connect(DB)
-
-
+const DB = process.env.MONGO_URI;
+mongoose.connect(DB)    
 
 
 app.use("/api/user", userRoutes);
@@ -36,4 +34,7 @@ const port=process.env.PORT || 5000;
 
 app.listen(port,()=>{
   console.log(`server is running on ${port}`);
-});  
+});    
+
+
+
