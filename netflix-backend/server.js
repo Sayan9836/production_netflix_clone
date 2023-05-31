085fb149a81ec5b3ab91a89f648d8e85b5e0e032
Sayan9836 <sayanmaity631@gmail.com>
2023-06-01 01:23:23 +0530

    sda

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -21,11 +21,11 @@ mongoose
   );
 
 
-// app.use(express.static(path.resolve(__dirname, 'netflix-frontend/build')))
+app.use(express.static(path.resolve(__dirname, 'netflix-frontend/build')))
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'netflix-frontend', 'build', 'index.html'))
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'netflix-frontend', 'build', 'index.html'))
+});
 
 
 
