432e33772ebecf408469c02191835fbe85d8b6e9
Sayan9836 <sayanmaity631@gmail.com>
2023-06-01 16:19:16 +0530

    asasdas

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -10,10 +10,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://main--mellow-torrone-a30106.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://6465ed6a698478000855cf32--mellow-torrone-a30106.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+  
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).end();
+    return;
+  }
+  
   next();
 });
 
