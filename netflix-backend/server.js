d58f3e0621b56ae8250abfb5ad8e69dc7e410b89
Sayan9836 <sayanmaity631@gmail.com>
2023-06-01 16:11:28 +0530

    dada

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://netflix-clone-nn3p.onrender.com/api/user');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
