126be68c84952475c5b0353c07e4cd9de838c80d
Sayan9836 <sayanmaity631@gmail.com>
2023-06-12 01:09:07 +0530

    sda

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -7,17 +7,23 @@ require('dotenv').config();
 const userRoutes = require("./routes/UserRoutes");
 const app = express();
 // app.use(corsAnywhere());
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://main--mellow-torrone-a30106.netlify.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+//   if (req.method === 'OPTIONS') {
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.status(200).end();
+//     return;
+//   }
+  
+//   next();
+// });
+
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://main--mellow-torrone-a30106.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(200).end();
-    return;
-  }
-  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
